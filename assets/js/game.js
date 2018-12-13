70362200c73e4ed70148c23c10b08a6f5e45b8b7
johndoeadams <johndoeadams@gmail.com>
2018-12-13 18:38:43 -0500

    Corrected logic
    adding some css, js changes, html changes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,44 +1,45 @@
-var c1=$("#c1").val();
-var c2=$("#c2").val();
-var c3=$("#c3").val();
-var c4=$("#c4").val();
-var c5=$("#c5").val();
 
-console.log(c3);
-console.log(c5);
+
+
 //GAME OBJECT
 var game={
   "gameNumber":0,
-  "currentNumber":0,
-  "clicky":[c1,c2,c3,c4]
+  "currentNumber":0
 }
-
-//add data attr or put in divs for each image. 
 
 //FUNCTIONS
 
 //reset the board and generate a new number
 function resetGame(){
+  game["gameNumber"]=0;
+  game["currentNumber"]=0;
   //generate number between 19 and 120 for the number to guess
   game["gameNumber"]=Math.floor(Math.random()*120)+19
-  console.log("Game Number reset: ", game["gameNumber"]);
+  $("#num").text("Random Number: "+game["gameNumber"]);
+  $("#num2").text("Your total number");
 //reset adding values to each crystal
-  game["clicky"].forEach(function(index){
+
     //assign a number to the value attribute for each image
-     index=Math.floor(Math.random*12)+1;
-     console.log(index);
-    //NOT CURRENTLY WORKING, DOES NOT ASSIGN
-  });
+    document.getElementById('c1').setAttribute("value", Math.floor(Math.random()*12)+1);
+    document.getElementById('c2').setAttribute("value", Math.floor(Math.random()*12)+1);
+    document.getElementById('c3').setAttribute("value", Math.floor(Math.random()*12)+1);
+    document.getElementById('c4').setAttribute("value", Math.floor(Math.random()*12)+1);
+
+      // $("c1").data("value", Math.floor(Math.random()*12)+1);
+      // $("c2").data("value", Math.floor(Math.random()*12)+1);
+      // $("c3").data("value", Math.floor(Math.random()*12)+1);
+      // $("c4").data("value", Math.floor(Math.random()*12)+1);
+
 }
 
 function add(clickedNumber){
-  game["currentNumber"]+=clickedNumber;
+  game["currentNumber"]+=parseInt(clickedNumber);
+  $("#num2").text("Total Number: "+game["currentNumber"]);
   checkGame();
-  console.log(clickedNumber);
 }
 //checks for winning condition
 function checkGame(){
-  if(game["gameNumber"]==game["currentNumber"]){
+  if(game["gameNumber"]===game["currentNumber"]){
     //win
     alert("You have won! The number was: "+game["gameNumber"])
     resetGame();
@@ -48,7 +49,6 @@ function checkGame(){
     alert("You have lost! The number was: "+game["gameNumber"])
     resetGame();
   }
-  console.log("Check Game");
 }
 
 //GAME SETUP
@@ -62,20 +62,24 @@ $(window).ready(function(){
 
   $("#c1").on("click", function(){
     //adds the currently assigned value to the current count
-  add(this.value);
+  add($(this).attr("value"));
+  console.log($(this).attr("value"))
   });
 
   $("#c2").on("click", function(){
       //adds the currently assigned value to the current count
-  add(this.value);
+  add($(this).attr("value"));
+  console.log($(this).attr("value"))
   });
 
   $("#c3").on("click", function(){
       //adds the currently assigned value to the current count
-  add(this.value);
+  add($(this).attr("value"));
+  console.log($(this).attr("value"))
   });
 
   $("#c4").on("click", function(){
       //adds the currently assigned value to the current count
-  add(this.value);
+  add($(this).attr("value"));
+  console.log($(this).attr("value"))
   });
